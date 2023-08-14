1da96dc44c841f71c89d94f18ff7beeda0e65efa
Abhi <abhirnw@gmail.com>
2023-08-14 08:00:22 +0530

    changes in controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,10 +4,21 @@ const { userService, emailService } = require("../services");
 const createUser = async (req, res) => {
   try {
     const reqBody = req.body;
+
+    const userExists = await userService.getUserByEmail(reqBody.email);
+    if (userExists) {
+      throw new Error("User already created by this email!");
+    }
+
+    const user = await userService.createUser(reqBody);
+    if (!user) {
+      throw new Error("Something went wrong, please try again or later!");
+    }
+
     res.status(200).json({
       success: true,
-      message: reqBody,
-      data: { reqBody },
+      message: "User create successfully!",
+      data: { user },
     });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
